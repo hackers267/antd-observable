3229a022c504032572e85b873cc265c373a2694b
silence_zhpf <silence_zhpf@aliyun.com>
2022-07-30 22:02:40 +0800

    feat: ✨ 添加toPage操作符
    ✨ 添加toPage操作符
    ✅ 添加toPage操作符的测试用例

--- src/operator/index.test.ts
+++ src/operator/index.test.ts
@@ -1,11 +1,28 @@
-import { of } from "rxjs";
-import { pickData } from "./index";
+import { of, throwError } from "rxjs";
+import { pickData, toPage } from "./index";
 
 describe("pickData", function () {
   it("should be right", function () {
     const observable = of({ success: true, data: "Hello" });
     observable.pipe(pickData()).subscribe((x) => {
       expect(x).toBe("Hello");
+    });
+  });
+});
+
+describe("toPage", function () {
+  it("should be right", function () {
+    const observable = of({ list: [], total: 34 });
+    observable.pipe(toPage()).subscribe((x) => {
+      expect(x).toStrictEqual({ total: 34, data: [], success: true });
+    });
+  });
+  it("should be with error", function () {
+    const observable = throwError(() => {
+      return new Error("Something is wrong!");
+    });
+    observable.pipe(toPage()).subscribe((x) => {
+      expect(x).toStrictEqual({ total: 0, data: [], success: true });
     });
   });
 });
--- src/operator/index.ts
+++ src/operator/index.ts
@@ -1,5 +1,20 @@
-import { map, pipe } from "rxjs";
+import { catchError, map, of, pipe } from "rxjs";
 
+type Page<T = any> = {
+  list: T[];
+  total: number;
+};
+
+export function toPage() {
+  return pipe(
+    map((page: Page) => {
+      const data = page.list;
+      const total = page.total;
+      return { success: true, total, data };
+    }),
+    catchError(() => of({ success: true, total: 0, data: [] }))
+  );
+}
 export function pickData() {
   return pipe(map<{ data?: any }, any>((x) => x?.data));
 }
