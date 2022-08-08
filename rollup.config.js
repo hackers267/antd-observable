091f9efd6d634336d1142ee55dcbad56068bfc22
silence_zhpf <silence_zhpf@aliyun.com>
2022-08-08 13:41:29 +0800

    feat: ✨ 添加commonjs打包

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,33 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 
-export default {
-  input: "src/index.ts",
-  output: {
-    dir: "dist",
-    format: "esm",
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/esm",
+      format: "esm",
+    },
+    plugins: [
+      typescript({
+        compilerOptions: { outDir: "dist/esm", declarationDir: "dist/esm" },
+      }),
+      commonjs(),
+    ],
+    external: ["rxjs"],
   },
-  plugins: [typescript(), commonjs()],
-  external: ["rxjs"],
-};
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "dist/cjs",
+      format: "cjs",
+    },
+    plugins: [
+      typescript({
+        compilerOptions: { outDir: "dist/cjs", declarationDir: "dist/cjs" },
+      }),
+      commonjs(),
+    ],
+    external: ["rxjs"],
+  },
+];
