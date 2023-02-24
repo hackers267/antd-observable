c459bbc5274e1de91ac95dda920ddcd6cd9cd0b4
silence_zhpf <silence_zhpf@aliyun.com>
2023-02-24 14:45:10 +0800

    chore: 🏷️ 修改类型错误

--- src/types/index.ts
+++ src/types/index.ts
@@ -17,7 +17,7 @@ type PageResult<T> = { success: boolean; data: T[]; total: number };
 
 export type ToDesc<T> = OperatorFunction<T, { data?: T; success: boolean }>;
 export type ToPage<T> = OperatorFunction<Page<T>, PageResult<T>>;
-export type ToVirtualPage<T> = OperatorFunction<T[], PageResult<T>>;
+export type ToVirtualPage<T> = OperatorFunction<T[] | undefined, PageResult<T>>;
 export type Sum = OperatorFunction<number, number>;
 export type SumBy = OperatorFunction<Record<string, any>, number>;
 export type PickData<T = any> = OperatorFunction<{ data?: T }, any>;
